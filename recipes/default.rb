44c58e1d5f9380036a2e708b1caac69dd7f0aa7b
Igor Aiestaran <igor.aiestaran@congenia.es>
2015-11-30 20:02:39 +0100

    apache log format

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,7 @@ if node[:opsworks][:layers].has_key?("php-app")
   node[:deploy].keys.each do |app|
     app_name = app['name']
     ["#{app}-access.log", "#{app}-ssl-access.log", "#{app}-error.log"].each do |logfile|
-      logs << {:log_location => "/var/log/apache2/#{logfile}", :log_group_name => node[:opsworks][:stack][:name], :log_stream_name => logfile, :datetime_format => "%b %d %H:%M:%S %Y"}
+      logs << {:log_location => "/var/log/apache2/#{logfile}", :log_group_name => node[:opsworks][:stack][:name], :log_stream_name => logfile, :datetime_format => "%d/%b/%Y:%H:%M:%S %z"}
     end
   end
 end
