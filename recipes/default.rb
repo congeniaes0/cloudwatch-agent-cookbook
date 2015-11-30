f597aea4a92c937200757f821d8268134da499f3
Igor Aiestaran <igor.aiestaran@congenia.es>
2015-11-30 17:10:25 +0100

    auto add apache error logs from each deployed app

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,11 +7,21 @@
 # both the template and the setup script download call the installation procedure because there might be changes
 # in the logs configuration when the agent is already installed; the bash resource will only be called once anyway
 
+logs = node[:congenia_common][:cloudwatch][:logs]
+if node['opsworks']['layers']['php-app']
+  node['deploy'].each do |app|
+    ["#{app}-error.log"].each do |logfile|
+      logs << {"log_location" : "/var/log/apache2/#{logfile}", "log_group_name" : node['opsworks']['stack']['name'], "log_stream_name" : logfile, "datetime_format" : "%b %d %H:%M:%S %Y"},
+    end
+  end
+end
+
 template "/etc/awslogs.conf" do
   source "awslogs.conf.erb"
   owner 'root'
   group 'root'
   mode 0600
+  variables :logs => logs
   notifies :create, 'remote_file[/root/awslogs-agent-setup.py]'
   notifies :run, 'bash[install_awslogs-agent]'
 end
