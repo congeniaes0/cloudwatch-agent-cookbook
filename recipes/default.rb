6092677f24ab0a5ca26dc2b192db677292e4a721
Igor Aiestaran <igor.aiestaran@congenia.es>
2015-11-30 18:20:31 +0100

    again strings to symbols as dict keys

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,7 +18,7 @@ end
 if node[:opsworks][:layers].has_key?("php-app")
   node[:deploy].each do |app|
     ["#{app}-access.log", "#{app}-error.log"].each do |logfile|
-      logs << {"log_location" => "/var/log/apache2/#{logfile}", "log_group_name" => node[:opsworks][:stack][:name], "log_stream_name" => logfile, "datetime_format" => "%b %d %H:%M:%S %Y"}
+      logs << {:log_location => "/var/log/apache2/#{logfile}", :log_group_name => node[:opsworks][:stack][:name], :log_stream_name => logfile, :datetime_format => "%b %d %H:%M:%S %Y"}
     end
   end
 end
