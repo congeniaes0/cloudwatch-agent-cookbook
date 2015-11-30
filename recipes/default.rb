512bda07730f4b328531344db1135f9dbeec20b2
Igor Aiestaran <igor.aiestaran@congenia.es>
2015-11-30 17:38:21 +0100

    fix syntax 2

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,7 +11,7 @@ logs = node[:congenia_common][:cloudwatch][:logs]
 if node['opsworks']['layers']['php-app']
   node['deploy'].each do |app|
     ["#{app}-access.log", "#{app}-error.log"].each do |logfile|
-      logs << {"log_location" => "/var/log/apache2/#{logfile}", "log_group_name" => node['opsworks']['stack']['name'], "log_stream_name" => logfile, "datetime_format" => "%b %d %H:%M:%S %Y"},
+      logs << {"log_location" => "/var/log/apache2/#{logfile}", "log_group_name" => node['opsworks']['stack']['name'], "log_stream_name" => logfile, "datetime_format" => "%b %d %H:%M:%S %Y"}
     end
   end
 end
